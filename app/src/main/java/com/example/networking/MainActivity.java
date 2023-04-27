75e18e7dcc3a4eb0c7ab2c8a3486c079c0338b09
Marcus Ahlberg <b22marah@student.his.se>
2023-04-27 14:51:42 +0200

    Updated and added new strings, a class (AuxData), fixed string converter and got started with the gson for-loop to iterate all values from listOfMountains...

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -7,16 +7,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 @SuppressWarnings("FieldCanBeLocal")
 public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
 
-    private final String JSON_URL = "HTTPS_URL_TO_JSON_DATA_CHANGE_THIS_URL";
+    private final String JSON_URL = "https://mobprog.webug.se/json-api?login=brom";
     private final String JSON_FILE = "mountains.json";
     private RecyclerView recyclerView;
     private ArrayList<Mountain> listOfMountains;
     private MyAdapter adapter;
+
+    @SuppressWarnings("SameParameterValue")
+    private String readFile(String fileName) {
+        try {
+            //noinspection CharsetObjectCanBeUsed
+            return new Scanner(getApplicationContext().getAssets().open(fileName), Charset.forName("UTF-8").name()).useDelimiter("\\A").next();
+        } catch (IOException e) {
+            return null;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +43,18 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
         adapter = new MyAdapter(listOfMountains);
         view.setLayoutManager(new LinearLayoutManager(this));
         view.setAdapter(adapter);
+
+        String s = readFile("mountains.json");
+        Log.d("MainActivity","The following text was found in textfile:\n\n"+s);
+
+        Gson gson = new Gson();
+        listOfMountains = gson.fromJson(s,ArrayList .class);
+
+        for (int i = 0; i < listOfMountains.size(); i++) {
+            Log.d("MainActivity","Hittade en fitta: " +i);
+        };
     }
+
 
     @Override
     public void onPostExecute(String json) {
--- app/src/main/java/com/example/networking/Mountain.java
+++ app/src/main/java/com/example/networking/Mountain.java
@@ -1,8 +1,15 @@
 package com.example.networking;
 
 public class Mountain {
+    private String ID;
     private String name;
+    private String type;
+    private String company;
+    private String location;
+    private String category;
     private int size;
+    private int cost;
+    private Auxdata Auxdata;
 
     public Mountain(String name, int size) {
 
